2797a90f3d3f9c039087d91ee9f7ff7320be8d85
Topi <nokoayzack@gmail.com>
2015-12-17 14:19:52 -0600

    feat: add some new features
    Add mongoose to the project and create db songs

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var less = require('less');
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/VectorBR");
+
+var songSchema = {
+  title:String,
+  Band:String,
+  imgUrl:String,
+};
+
+var Song = mongoose.model("Song", songSchema);
 
 var app = express();
 
@@ -8,10 +19,32 @@ app.set("view engine", "jade");
 app.use(express.static("public"));
 
 app.get("/",function(req, res){
+
+  /*var data = {
+      title: "No es por ti",
+      Band: "Comisario Pantera",
+      imgUrl: "data.png"
+  }
+
+  var song = new Song(data);
+
+  song.save(function(err){
+    console.log(song);
+  });*/
+
   res.render("index");
 });
+
 app.get("/contacto", function(req, res){
   res.render("contacto");
+});
+
+app.get("/canciones", function(req, res){
+  res.render("songs/songs");
+});
+
+app.get("/songs/new", function(req, res){
+  res.render("songs/new");
 });
 
 app.listen(1337);
